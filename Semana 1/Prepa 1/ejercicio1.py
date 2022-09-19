ebb063cd044c02276bac4da1f1bdbb7cca2ae386
MariaLauraIsea <m.isea@correo.unimet.edu.ve>
2022-09-19 10:16:45 -0400

    semana 2

---
+++ Semana 1/Prepa 1/ejercicio1.py
@@ -0,0 +1,8 @@
+saldo_inicial= 3480
+print(f"saldo actual a la fecha (2020/04/11): $ {saldo_inicial}")
+#restar $96 por compra
+saldo_restante= saldo_inicial-96
+print("se ha retirado $96")
+print(f"saldo actual a la fecha (2020/04/11): $ {saldo_restante}")
+
+ 
